67d49ab8a58a1ce67fd4cfb3a9a83d0c5b6b8b4a
Long Pham <phlexile@gmail.com>
2021-07-06 22:52:02 +0700

    buon

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const {check, validationResult} = require('express-validator/check');
 const User = require('../models/User');
 // @route POST api/users.
 // @desc Register a user.
 // @access Public.
-router.post('/', [
-  check('name', 'please add name')
-    .not()
-    .isEmpty(),
-  check('email', 'please add email')
-    .isEmail(),
-  check('password', 'please add password with 6 characters')
-    .isLength({ min: 6 })
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) { // errors is not Empty!.
-    return res.status(400).json({ errors: errors.array() })
+router.post(
+  '/',
+  [
+    check('name', 'please add name').not().isEmpty(),
+    check('email', 'please add email').isEmail(),
+    check('password', 'please add password with 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // errors is not Empty!.
+      return res.status(400).json({errors: errors.array()});
+    }
+    res.send(req.body);
   }
-  res.send(req.body);
-});
+);
 
 module.exports = router;
